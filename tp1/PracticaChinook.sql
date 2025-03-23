ce6be5867b64f3d56bc48274f0101d7997ea5f4c
Hernan Silva <hernansilva21@yahoo.com.ar>
2025-03-23 11:58:54 -0300

    Agrega queries

--- tp1/PracticaChinook.sql
+++ tp1/PracticaChinook.sql
@@ -3,20 +3,54 @@ SELECT * FROM album;
 -- Selecciona todos los géneros únicos de la tabla Genres.
 SELECT DISTINCT * FROM genre;
 -- Cuenta el número de pistas por género.
-SELECT genre.name, COUNT(*) FROM track INNER JOIN genre ON genre.genre_id = track.genre_id GROUP BY genre.name;
+SELECT genre.name, COUNT(*) 
+FROM track 
+INNER JOIN genre ON genre.genre_id = track.genre_id 
+GROUP BY genre.name;
 -- Encuentra la longitud total (en milisegundos) de todas las pistas para cada álbum.
-
+SELECT album.title, SUM(milliseconds) AS "Longitud total" 
+FROM track 
+INNER JOIN album ON album.album_id = track.album_id 
+GROUP BY track.album_id, album.title;
 -- Lista los 10 álbumes con más pistas.
-
+SELECT album.title, COUNT(track.track_id) AS "Total Pistas" 
+FROM track 
+INNER JOIN album ON album.album_id = track.album_id 
+GROUP BY track.album_id, album.title 
+ORDER BY COUNT(track.track_id) DESC 
+LIMIT 10;
 -- Encuentra la longitud promedio de la pista para cada género.
-
+SELECT genre.name, AVG(milliseconds) AS "Longitud promedio" 
+FROM track 
+INNER JOIN genre ON genre.genre_id = track.genre_id 
+GROUP BY track.genre_id, genre.name;
 -- Para cada cliente, encuentra la cantidad total que han gastado.
-
+SELECT customer_id, SUM(total) AS "Total gastado" 
+FROM invoice 
+GROUP BY customer_id;
 -- Para cada país, encuentra la cantidad total gastada por los clientes.
-
+SELECT billing_country, SUM(total) AS "Total gastado" 
+FROM invoice 
+GROUP BY billing_country;
 -- Clasifica a los clientes en cada país por la cantidad total que han gastado.
-
+SELECT billing_country, 
+       SUM(total) AS "Total gastado", 
+       CASE WHEN SUM(total) > 200 THEN 'A' 
+            WHEN SUM(total) BETWEEN 100 AND 200 
+            THEN 'B' ELSE 'C'
+       END AS "Clasificacion" 
+FROM invoice 
+GROUP BY billing_country;
 -- Para cada artista, encuentra el álbum con más pistas y clasifica a los artistas por este número.
+WITH track_count AS (
+     SELECT artist.name, album.title, COUNT(track.track_id) AS cantidad FROM track 
+     INNER JOIN album ON album.album_id = track.album_id
+     INNER JOIN artist ON artist.artist_id = album.artist_id
+     GROUP BY artist.name, album.album_id)
+SELECT name, title, MAX(cantidad) OVER (PARTITION BY name, title)
+FROM track_count
+
+
 
 -- Selecciona todas las pistas que tienen la palabra "love" en su título.
 
