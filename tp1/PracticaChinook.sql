ae696f965e79b21aa175248978e427aa43b8c89a
Hernan Silva <hernansilva21@yahoo.com.ar>
2025-03-23 22:55:04 -0300

    mas queries

--- tp1/PracticaChinook.sql
+++ tp1/PracticaChinook.sql
@@ -34,33 +34,51 @@ FROM invoice
 GROUP BY billing_country;
 -- Clasifica a los clientes en cada país por la cantidad total que han gastado.
 SELECT billing_country, 
-       SUM(total) AS "Total gastado", 
-       CASE WHEN SUM(total) > 200 THEN 'A' 
-            WHEN SUM(total) BETWEEN 100 AND 200 
-            THEN 'B' ELSE 'C'
-       END AS "Clasificacion" 
+     SUM(total) AS "Total gastado", 
+       NTILE(3) OVER (ORDER BY SUM(total) DESC) AS "Clasificacion"
 FROM invoice 
 GROUP BY billing_country;
+
 -- Para cada artista, encuentra el álbum con más pistas y clasifica a los artistas por este número.
 WITH track_count AS (
-     SELECT artist.name, album.title, COUNT(track.track_id) AS cantidad FROM track 
+     SELECT 
+          artist.name as artist_name,
+          album.title as album_title,
+          COUNT(track.track_id) as track_count,
+          RANK() OVER (PARTITION BY artist.name ORDER BY COUNT(track.track_id) DESC) as rank
+     FROM track 
      INNER JOIN album ON album.album_id = track.album_id
      INNER JOIN artist ON artist.artist_id = album.artist_id
-     GROUP BY artist.name, album.album_id)
-SELECT name, title, MAX(cantidad) OVER (PARTITION BY name, title)
+     GROUP BY artist.name, album.title
+)
+SELECT 
+     artist_name,
+     album_title,
+     track_count,
+     DENSE_RANK() OVER (ORDER BY track_count DESC) as artist_rank
 FROM track_count
-
-
+WHERE rank = 1
+ORDER BY track_count DESC;
 
 -- Selecciona todas las pistas que tienen la palabra "love" en su título.
+SELECT * FROM track WHERE name LIKE '% love %';
 
 -- Selecciona a todos los clientes cuyo primer nombre comienza con 'A'.
+SELECT * FROM customer WHERE first_name LIKE 'A%';
 
 -- Calcula el porcentaje del total de la factura que representa cada factura.
-
+SELECT invoice_id, total, total / SUM(total) OVER () AS "Porcentaje total"
+FROM invoice;
 -- Calcula el porcentaje de pistas que representa cada género.
-
+SELECT genre.name, COUNT(*) AS "Total pistas", COUNT(*) / SUM(COUNT(*)) OVER () AS "Porcentaje total"
+FROM track
+INNER JOIN genre ON genre.genre_id = track.genre_id
+GROUP BY genre.name;
 -- Para cada cliente, compara su gasto total con el del cliente que gastó más.
+SELECT customer_id, SUM(total) AS "Total gastado",
+       MAX(SUM(total)) OVER () AS "Maximo gastado"
+FROM invoice
+GROUP BY customer_id;
 
 -- Para cada factura, calcula la diferencia en el gasto total entre ella y la factura anterior.
 
